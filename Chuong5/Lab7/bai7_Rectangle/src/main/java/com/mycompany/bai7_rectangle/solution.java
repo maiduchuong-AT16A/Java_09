86409086c532dfb361cd1c5ef2d48584ac077410
AT200452-LeThiMinhTam <mtam7883@gmail.com>
2024-05-11 10:10:24 +0700

    Le Thi Minh Tam

---
+++ Chuong5/Lab7/bai7_Rectangle/src/main/java/com/mycompany/bai7_rectangle/solution.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bai7_rectangle;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class solution {
+    public static void main(String[] args) {
+        HinhChuNhat hinhchunhat = new HinhChuNhat();
+        hinhchunhat.input();
+        hinhchunhat.output();
+    }
+}
