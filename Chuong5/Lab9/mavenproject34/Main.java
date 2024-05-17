cad6d91e5438d4abfa8184d66a7180b4f6cebc91
AT200447 - Đào Thị Minh Phương <minhphuong24012912@gmail.com>
2024-05-17 16:25:38 +0700

    Dao Thi Minh Phuong

---
+++ Chuong5/Lab9/mavenproject34/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject34;
+
+/**
+ *
+ * @author dell-vip
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        MaTran mt = new MaTran();
+        mt.inputInfo();
+        mt.showInfo();
+        mt.xoayPhai90();
+        mt.xoay180();
+        mt.xoayTrai90();
+    }
+}
