a0154552c8b68c2689d453fa83be7979a8c5c68c
CT080243 <quanminhnguyen1112@gmail.com>
2024-05-17 17:04:10 +0700

    Nguyen Minh Quan

---
+++ Chuong6/test2/src/main/java/javacore/basic/lesson06/Solution.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javacore.basic.lesson06;
+
+/**
+ *
+ * @author quanm
+ */
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); int T = sc.nextInt();
+        for(int i = 1; i <= T; i++)
+        {
+            int n = sc.nextInt(); 
+            double[] a = new double[n];
+            for(int j = 0; j < n; j++)
+            {
+                a[j] = sc.nextDouble();
+            }
+            sc.nextLine();
+            String str1 = sc.nextLine(); String str2 = sc.nextLine(); int k = sc.nextInt();
+            System.out.printf("Case #%d:\n", T);
+            System.out.printf("Sum: %f\n",Project.getSum(a));
+            System.out.printf("Min: %f\n",Project.getMin(a));
+            System.out.printf("Max: %f\n", Project.getMax(a));
+            System.out.printf("To upper: %s\n", Project.toUpper(str1));
+            System.out.printf("To lower: %s\n", Project.toLower(str2));
+            System.out.printf("To upper first char: %s - %s\n", Project.toUpperFirstChar(str1), Project.toUpperFirstChar(str2));
+            System.out.printf("Fibonacci(%d): %d",k ,Project.getFibonacci(k));
+        }
+    }
+}
