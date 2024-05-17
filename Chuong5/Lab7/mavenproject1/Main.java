3188c661b5bf9825856be657641334fcbaf0b6db
AT190101-Trinh Chi An <134771462+TrinhChiAn@users.noreply.github.com>
2024-05-17 16:12:22 +0700

    Trịnh chí an submit bài 7

---
+++ Chuong5/Lab7/mavenproject1/Main.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package leetcode.mavenproject1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tuan Anh
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+         MaTran mt1 = new MaTran();
+          
+          mt1.input();
+          mt1.output();
+          mt1.xoaytrai90();
+          mt1.output();
+          mt1.xoayphai90();
+          mt1.output();
+         
+     }
+
+}
