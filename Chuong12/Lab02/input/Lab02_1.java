5ada381c8c4765bd3f6cc3dbeb2a1e1b2801ab87
AT200447 - Đào Thị Minh Phương <minhphuong24012912@gmail.com>
2024-06-21 20:56:15 +0700

    Dao Thi Minh Phuong nop bai

---
+++ Chuong12/Lab02/input/Lab02_1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.lab02_1;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class Lab02_1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        for (int t = 1; t <= T; t++) {
+            int day1 = sc.nextInt();
+            int month1 = sc.nextInt();
+            int year1 = sc.nextInt();
+            LocalDate firstDay = LocalDate.of(year1, month1, day1);
+            int day2 = sc.nextInt();
+            int month2 = sc.nextInt();
+            int year2 = sc.nextInt();
+            LocalDate secondDay = LocalDate.of(year2, month2, day2);
+            Period kc = Period.between(firstDay, secondDay);
+            System.out.println("Case #" + t + ":");
+            
+            
+            System.out.printf("Difference between %02d/%02d/%04d and %02d/%02d/%04d: \n%d Year(s), %d Month(s), %d Day(s)\n", day1, month1, year1, day2, month2, year2, Math.abs(kc.getYears()), Math.abs(kc.getMonths()), Math.abs(kc.getDays()));
+        }
+    }
+}
