1ad8d7715f447a355716f56895680b477930a3e7
CT080243 <quanminhnguyen1112@gmail.com>
2024-06-28 21:01:55 +0700

    Nguyen Minh Quan

--- Chuong13/lab1/Lab01_Main.java
+++ Chuong13/lab1/Lab01_Main.java
@@ -1,105 +1,92 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Lab_01;
-
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+package com.mycompantes.bai_13_baitap;
 
 /**
  *
- * @author DELL
+ * @author quanm
  */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lab01_Main {
 
-    static Scanner sc = new Scanner(System.in);
-    static LinkedList<Integer> dsach = new LinkedList<>();
-    
-    static void func01()
-    {
-        char kiTu = sc.next().charAt(0);
-        switch (kiTu) {
-            case 'F' -> dsach.add(0, sc.nextInt());
-            case 'L' -> dsach.add(dsach.size(), sc.nextInt());
-            default -> {
-                int x,y;
-                y = sc.nextInt();
-                x = sc.nextInt();
-                dsach.add(y, x);
-            }
-        }
-    }
-    
-    static void func02()
-    {
-        int count=0, x = sc.nextInt();
-        for (int i=0; i<dsach.size(); i++)
-        {
-            if (dsach.get(i)==x)
-            {
-                dsach.remove(i);
-                count++;
-            }
-        }
-        System.out.println(count);
-    }
-    
-    static void func03()
-    {
-        int count=0, x = sc.nextInt();
-        for (int i=0; i<dsach.size(); i++)
-        {
-            if (dsach.get(i)==x)
-            {
-                count++;
-            }
-        }
-        System.out.println(count);
-    }
-    
-    static void func04()
-    {
-        int x, y;
-        x = sc.nextInt();
-        y = sc.nextInt();
-        for (int i=x; i<=y; i++)
-            System.out.print(dsach.get(i)+" ");
-        System.out.println("");
-    }
-    
-    static void func05()
-    {
-        for (int i=0; i<dsach.size(); i++)
-            System.out.printf(dsach.get(i)+" ");
-        System.out.println("");
-    }
-    
     public static void main(String[] args) {
-        int n, N = sc.nextInt();
-        for (n=1; n<=N; n++)
-        {
-            short choice = sc.nextShort();
-            switch (choice)
-            {
-                case 1:
-                    func01();
-                    break;
-                case 2:
-                    func02();
-                    break;
-                case 3:
-                    func03();
-                    break;
-                case 4:
-                    func04();
-                    break;
-                case 5:
-                    func05();
-                    break;
-            }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x;
+        int y;
+        int count = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int e = 0; e < n; e++) {
+             int choice = sc.nextInt();
+             sc.nextLine();
+             switch(choice)
+             {
+                 case 1:
+                     char c = sc.next().charAt(0);
+                     
+                     sc.nextLine();
+                     if(c == 'F'){
+                         x = sc.nextInt();
+                         sc.nextLine();
+                         list.add(0, x);
+                     }
+                     else if(c == 'L'){
+                         x = sc.nextInt();
+                         sc.nextLine();
+                         list.add(list.size(), x);
+                     }
+                     else if(c == 'P'){
+                         y = sc.nextInt();
+                         x = sc.nextInt();
+                         sc.nextLine();
+                         if(list.size() < y)
+                         {
+                             list.add(list.size(), x);
+                         }
+                         else{
+                             list.add(y, x);
+                         }
+                     }
+                     break;
+                 case 2:
+                     x = sc.nextInt();
+                     sc.nextLine();               
+                     for(int i = 0; i < list.size(); i++){
+                         if(list.get(i) == x){
+                             list.remove(i);
+                             count++;
+                         }
+                     }
+                     System.out.println(count);
+                     count = 0;
+                     break;
+                 case 3:
+                     x = sc.nextInt();
+                     for(int i = 0; i < list.size(); i++){
+                         if(list.get(i) == x){
+                             count++;
+                         }
+                     }
+                     System.out.println(count);
+                     count = 0;
+                     break;
+                 case 4:
+                     x = sc.nextInt();
+                     y = sc.nextInt();
+                     for(int i = x; i <= y; i++){
+                         System.err.printf("%d ", list.get(i));
+                         
+                     }System.out.println("");
+                     break;
+                 case 5:
+                     for(int i = 0; i < list.size(); i++){
+                         System.out.printf("%d ", list.get(i));
+                         
+                     }System.out.println("");
+             }
         }
     }
-    
 }
