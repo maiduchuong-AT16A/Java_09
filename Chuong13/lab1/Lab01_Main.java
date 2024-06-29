c6dc669247c7703b5549a88d0e214eac48d02b86
Thiều <thieucx118@gmail.com>
2024-06-29 08:12:12 +0700

    Nguyen Gia Thieu

--- Chuong13/lab1/Lab01_Main.java
+++ Chuong13/lab1/Lab01_Main.java
@@ -1,92 +1,109 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompantes.bai_13_baitap;
+package com.mycompany.bai2;
+
+import static java.lang.constant.ConstantDescs.NULL;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 /**
  *
- * @author quanm
+ * @author tuan anh
  */
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Lab01_Main {
-
+    static Scanner sc = new Scanner(System.in);
+    static LinkedList<Integer> list = new LinkedList<>();
+    static void case_1()
+    {
+        char kitu = sc.next().charAt(0);
+        int X;
+        switch (kitu) {
+            case 'F':
+                X = sc.nextInt();
+                list.addFirst(X);
+                break;
+            case 'L':
+                X = sc.nextInt();
+                list.addLast(X);
+                break;
+            default:
+                int Y = sc.nextInt();
+                X = sc.nextInt();
+                if(Y<list.size()){
+                    list.add(Y,X);
+                }
+                else
+                {
+                    list.add(X);
+                }
+        }
+    }
+    static void case_2()
+    {
+        int X = sc.nextInt();
+        for(int i=0; i<list.size(); i++) 
+        {
+            if(list.get(i)==X)
+                list.remove(i);
+        }
+        System.out.println(X);
+    }
+    static void case_3()
+    {
+        int dem =0;
+        int X = sc.nextInt();
+        for(int i=0; i<list.size();i++)
+        {
+            if(list.get(i)==X)
+            {
+                dem++;
+            }
+        }
+        System.out.println(dem);
+    }
+    static void case_4()
+    {
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        for(int i=X;i<=Y;i++)
+        {
+            System.out.printf("%d ", list.get(i));
+        }
+    }
+    static void case_5()
+    {
+        for(int i=0;i<list.size();i++)
+        {
+            System.out.printf("%d ", list.get(i));
+        }
+    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int x;
-        int y;
-        int count = 0;
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int e = 0; e < n; e++) {
-             int choice = sc.nextInt();
-             sc.nextLine();
-             switch(choice)
-             {
-                 case 1:
-                     char c = sc.next().charAt(0);
-                     
-                     sc.nextLine();
-                     if(c == 'F'){
-                         x = sc.nextInt();
-                         sc.nextLine();
-                         list.add(0, x);
-                     }
-                     else if(c == 'L'){
-                         x = sc.nextInt();
-                         sc.nextLine();
-                         list.add(list.size(), x);
-                     }
-                     else if(c == 'P'){
-                         y = sc.nextInt();
-                         x = sc.nextInt();
-                         sc.nextLine();
-                         if(list.size() < y)
-                         {
-                             list.add(list.size(), x);
-                         }
-                         else{
-                             list.add(y, x);
-                         }
-                     }
-                     break;
-                 case 2:
-                     x = sc.nextInt();
-                     sc.nextLine();               
-                     for(int i = 0; i < list.size(); i++){
-                         if(list.get(i) == x){
-                             list.remove(i);
-                             count++;
-                         }
-                     }
-                     System.out.println(count);
-                     count = 0;
-                     break;
-                 case 3:
-                     x = sc.nextInt();
-                     for(int i = 0; i < list.size(); i++){
-                         if(list.get(i) == x){
-                             count++;
-                         }
-                     }
-                     System.out.println(count);
-                     count = 0;
-                     break;
-                 case 4:
-                     x = sc.nextInt();
-                     y = sc.nextInt();
-                     for(int i = x; i <= y; i++){
-                         System.err.printf("%d ", list.get(i));
-                         
-                     }System.out.println("");
-                     break;
-                 case 5:
-                     for(int i = 0; i < list.size(); i++){
-                         System.out.printf("%d ", list.get(i));
-                         
-                     }System.out.println("");
-             }
+        int T = sc.nextInt();
+        int d;
+        for(int t=0; t<T; t++)
+        {
+            d = sc.nextInt();
+            switch (d) {
+                case 1:
+                    case_1();
+                    break;
+                case 2:
+                    case_2();
+                    break;
+                case 3:
+                    case_3();
+                    break;
+                case 4:
+                    case_4();
+                    System.out.println("");
+                    break;
+                case 5:
+                case_5();
+                    System.out.println("");
+                break;
+            }
         }
     }
 }
